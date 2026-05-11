f3472dfce47ec7ebe6477fb4723ab3486d5f080c
Uday Senapati <udaysenapati6878@gmail.com>
2026-05-11 21:02:50 +0530

    feat: update contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -83,7 +83,7 @@ export default function Page() {
         </section>
 
         {/* CONTACT */}
-        <section className="stack-panel absolute inset-0 h-screen w-full bg-[#141414] flex items-center overflow-hidden">
+        <section className="z-10 stack-panel absolute inset-0 h-screen w-full bg-[#141414] flex items-center overflow-hidden">
           <div className="w-full px-6 sm:px-10 md:px-16 xl:px-40 pt-16 sm:pt-20 flex flex-col md:flex-row gap-8 md:gap-16">
 
             {/* LEFT */}
