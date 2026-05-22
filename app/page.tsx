96553eea72774b4e8c92a9e3ece17cc26a5f4907
Uday Senapati <udaysenapati6878@gmail.com>
2026-05-22 19:58:20 +0530

    update content

--- app/page.tsx
+++ app/page.tsx
@@ -94,7 +94,7 @@ export default function Page() {
             className="absolute inset-0 w-full h-full object-cover z-10 rotate-"
             autoPlay muted loop playsInline preload="auto"
           >
-            <source src="https://res.cloudinary.com/dthpzuhja/video/upload/v1779457224/Timeline_1_xf8b9p.mov" type="video/mp4" />
+            <source src="https://res.cloudinary.com/dthpzuhja/video/upload/v1779459932/Timeline_1_xf8b9p_edzpnr.mp4" type="video/mp4" />
           </video>
 
           <div className="absolute inset-0 bg-black/40" />
